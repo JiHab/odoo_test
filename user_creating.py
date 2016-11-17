223234a7534f535a94099d07db0005814b2c3b6e
jihab <vladimir.mail@ukr.net>
2016-11-17 02:00:18 +0200

    odoo

---
+++ user_creating.py
@@ -0,0 +1,61 @@
+import xmlrpc.client
+
+url = 'http://91.218.214.19:8071'
+db = 'demoerp'
+username = 'admin'
+password = 'admin'
+
+new_login = 'login'
+new_username = 'Тестовый пользователь'
+new_user_city = 'Киев'
+new_user_street = 'Хрещатик'
+
+
+def login_is_valid(login, db, uid, password):
+    login = login.strip()
+    if login:
+        finded = models.execute_kw(db, uid, password,
+                                   "res.users", 'search',
+                                   [[['login', '=', login]]])
+        if finded:
+            print('Пользователь с таким логином уже существует')
+            return False, None
+        else:
+            return True, login
+    else:
+        print('Некорретный логин')
+        return False, None
+
+# noinspection PyBroadException
+try:
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    uid = common.authenticate(db, username, password, {})
+except ConnectionRefusedError:
+    print('Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение')
+    quit()
+except:
+    print('Ошибка при аутентификации')
+    quit()
+
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+
+isvalid, login = login_is_valid(new_login, db, uid, password)
+if not isvalid:
+    quit()
+
+id = models.execute_kw(db, uid, password, 'res.partner', 'create', [{
+    'name': new_username,
+    'city': new_user_city,
+    'street': new_user_street,
+}])
+
+u_id = models.execute_kw(db, uid, password, 'res.users', 'create', [{
+    'name': new_username,
+    'login': login,
+    'partner_id': id
+}])
+
+models.execute_kw(db, uid, password, 'res.partner', 'write', [[id], {
+    'user_id': u_id
+}])
+
